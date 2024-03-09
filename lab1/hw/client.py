47c43d9b570a96fc4f953ede36582195c5e98037
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-03-09 01:29:20 +0100

    update

---
+++ lab1/hw/client.py
@@ -0,0 +1,85 @@
+import socket
+import threading
+
+ASCII_ART = """
+,---,---,---,---,---,---,---,---,---,---,---,---,---,-------,
+| ~ | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | [ | ] | <-    |
+|---'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-----|
+| ->| | " | , | . | P | Y | F | G | C | R | L | / | = |  \  |
+|-----',--',--',--',--',--',--',--',--',--',--',--',--'-----|
+| Caps | A | O | E | U | I | D | H | T | N | S | - |  Enter |
+|------'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'-,-'--------|
+|        | ; | Q | J | K | X | B | M | W | V | Z |          |
+|------,-',--'--,'---'---'---'---'---'---'-,-'---',--,------|
+| ctrl |  | alt |                          | alt  |  | ctrl |
+'------'--'-----'--------------------------'------'--'------'
+"""
+closed = False
+
+
+def receive(client_socket: socket.socket):
+    global closed
+    while True:
+        try:
+            message = client_socket.recv(1024).decode("utf-8")
+            if not message:
+                closed = True
+                print("Server closed connection")
+                break
+            print(f"\n<<< {message}")
+        except Exception as e:
+            print(e)
+            break
+
+
+def receive_udp(udp_socket: socket.socket):
+    while True:
+        try:
+            buff, _ = udp_socket.recvfrom(1024)
+            print(f"\n<<<\n{buff.decode("utf-8")}")
+        except Exception as e:
+            print(e)
+            break
+
+
+def client():
+    addr_server = ("127.0.0.1", 9009)
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(addr_server)
+
+    receive_thread = threading.Thread(target=receive, args=(client_socket,))
+    receive_thread.daemon = True
+    receive_thread.start()
+    #
+    #
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(client_socket.getsockname())
+
+    udp_thread = threading.Thread(target=receive_udp, args=(udp_socket,))
+    udp_thread.daemon = True
+    udp_thread.start()
+
+    global closed
+    while True:
+        try:
+            message = input(">>> ")
+            if closed:
+                break
+            if message == "U":
+                udp_socket.sendto(ASCII_ART.encode("utf-8"), addr_server)
+            else:
+                client_socket.send(message.encode("utf-8"))
+
+        except KeyboardInterrupt:
+            break
+
+        except Exception as e:
+            print(e)
+            break
+
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    client()
--- lab1/hw/server.py
+++ lab1/hw/server.py
@@ -11,6 +11,9 @@ class Client:
     address: str
 
 
+lock = threading.Lock()
+
+
 def handle_client(client: Client, clients: list[Client], pool: set[int]):
     client.socket.send(f"Connected. You are {client.name}.".encode("utf-8"))
     while True:
@@ -19,8 +22,9 @@ def handle_client(client: Client, clients: list[Client], pool: set[int]):
             if not message:
                 print(f"Client {client.name} disconnected")
                 client.socket.close()
-                clients.remove(client)
-                pool.add(client.id)
+                with lock:
+                    clients.remove(client)
+                    pool.add(client.id)
                 break
             else:
                 print(f"Received message from {client.name}: {message}")
@@ -32,31 +36,54 @@ def handle_client(client: Client, clients: list[Client], pool: set[int]):
         except Exception as e:
             print(f"Error handling client: {client.name}. {e}")
             client.socket.close()
-            clients.remove(client)
-            pool.add(client.id)
+            with lock:
+                clients.remove(client)
+                pool.add(client.id)
             break
 
 
-def server(addr=("", 9999), max_pool=2):
+def handle_udp(udp_socket: socket.socket, clients: list[Client]):
+    while True:
+        try:
+            buff, addr = udp_socket.recvfrom(1024)
+            print(f"Received message\n{buff.decode("utf-8")}")
+            for client in clients:
+                if client.address != addr:
+                    udp_socket.sendto(buff, client.address)
+
+        except Exception as e:
+            print(e)
+            break
+
+
+def server(max_pool=2):
+    addr = ("127.0.0.1", 9009)
     server_socket = socket.create_server(addr, family=socket.AF_INET)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind(addr)
+
     clients: list[Client] = []
     pool = set(range(max_pool))
 
     print("Chat server is running...")
 
+    udp_thread = threading.Thread(target=handle_udp, args=(udp_socket, clients))
+    udp_thread.daemon = True
+    udp_thread.start()
+
     while True:
         try:
             client_socket, client_address = server_socket.accept()
-
             print(f"New connection from {client_address}")
+
             if len(pool) == 0:
-                client_socket.send(f"Connection rejected. Pool is empty.".encode("utf-8"))
+                client_socket.send("Connection rejected. Pool is empty.".encode("utf-8"))
                 client_socket.close()
                 print(f"Connection rejected. Pool is empty.")
-
             else:
-                client = Client(id := pool.pop(), f"USER-{id}", client_socket, client_address)
-                clients.append(client)
+                with lock:
+                    client = Client(id := pool.pop(), f"USER-{id}", client_socket, client_address)
+                    clients.append(client)
 
                 client_thread = threading.Thread(target=handle_client, args=(client, clients, pool))
                 client_thread.daemon = True
