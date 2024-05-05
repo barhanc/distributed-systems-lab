3b7fd4cc450585bb26f5c2074f1f363cfb6b64dd
Bartosz Hanc <bartosz.hanc02@gmail.com>
2024-05-05 15:43:25 +0200

    update

--- lab4-5/ex1/client.py
+++ lab4-5/ex1/client.py
@@ -52,8 +52,8 @@ def client(stub):
     subscriptions: list[gen.event_sub_pb2.ArticleType] = []
     handle_threads = []
 
-    try:
-        while True:
+    while True:
+        try:
             command = input(">>> ")
             matched = False
 
@@ -86,8 +86,12 @@ def client(stub):
             if not matched and len(command) > 0:
                 print(HELP_PROMPT)
 
-    except KeyboardInterrupt:
-        exit(0)
+        except KeyboardInterrupt:
+            exit(0)
+
+        except Exception as e:
+            print(e)
+            exit(0)
 
 
 if __name__ == "__main__":
